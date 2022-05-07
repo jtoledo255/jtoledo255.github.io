99c5f968c8fb3718762c341d5d352822d3b66fe1
jtoledo255 <67405240+jtoledo255@users.noreply.github.com>
2022-05-07 09:58:48 -0500

    Add files via upload

---
+++ Calculations/pressure_calc.m
@@ -0,0 +1,16 @@
+density = 1; %kg/m^3
+c = 1540;
+U = 1; %m/s
+w = 0;
+
+wavelength = 2.5*10^-6;
+k = 1/wavelength;
+a = 3*10^-3;
+r = [0:.001:1];
+for t = 1:1000
+pressure{t} = density*c*U*exp(1i*w*t)*(exp(-1i*k*r)-exp(-1i*k*sqrt(r.^2+a^2)));
+end
+
+figure()
+plot(r,abs(pressure{1,1}))
+saveas(gcf,'C:\Users\jtole\Box\BADER\BADER Students\2021\Toledo-Urena\Git_Test\hello-world\Test_Repo\manuscript_images_test\pressure.png')
